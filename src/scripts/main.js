97bbdd608a9e8e9b1faa6fbec8f74c67ad631510
Gabriel Gontijo <96489350+Gonteejo@users.noreply.github.com>
2023-10-15 11:13:01 -0300

    Adicionado contador até a hora do evento.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    alert("Ready!");
-}, false);
+AOS.init();
+
+const dataDoEvento = new Date("Oct 30, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const constaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+    const segundoEmMs = 1000;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / segundoEmMs);
+    
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    
+    if (distanciaAteOEvento < 0) {
+        clearInterval(constaAsHoras);
+        document.getElementsByClassName("hero__text")[0].innerHTML = "Para esse ano você chegou atrasado, tente ano que vem!";
+    }
+}, 500);
